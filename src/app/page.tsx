a887fcc88545fe82cf9f0a99ede77569fc2bebc1
Andrey Dmitriew <hoffartisad@gmail.com>
2025-06-15 19:48:37 +0300

    fix Vercel error.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-'use client';
-
+import { Suspense } from "react";
 import CarListing from "@/widgets/car-listing/CarListing";
 
 export default function HomePage() {
-  return <CarListing />;
+  return (
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <CarListing />
+    </Suspense>
+  );
 }
